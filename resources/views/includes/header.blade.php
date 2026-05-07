aaad30b4f20708377154364e653899c28a9a035a
Mjayy <riemazoey14@gmail.com>
2026-05-08 01:16:12 +0800

    done changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,15 @@
-<header class="plp-header">
+<header class="plp-header" style="background-color: #1a5c2a;">
     <div class="container-fluid d-flex align-items-center px-4 py-2">
         <a href="{{ url('/') }}" class="d-flex align-items-center text-decoration-none">
-            <img src="{{ asset('img/plplogo.png') }}" alt="PLP Logo" class="plp-header-logo">
+            <img src="{{ asset('img/plplogoo.png') }}" alt="PLP Logo" class="plp-header-logo">
             <div class="header-title-text ms-3 d-none d-md-block">
+                <div style="font-size: 1.25rem; font-weight: 550; color: #e8d34ddc; line-height: 1; text-transform: uppercase; font-family: Arial, sans-serif; padding-bottom: 4px;">
+                    PAMANTASAN NG LUNGSOD NG PASIG
+                </div>
+                <div style="border-top: 1px solid #fffadbc0;"></div>
+                <div style="font-size: 0.85rem; font-weight: 400; color: #d0d0d0df; letter-spacing: 0.08em; text-transform: uppercase; font-family: Arial, sans-serif; padding-top: 4px;">
+                    UNIVERSITY OF PASIG CITY
+                </div>
             </div>
         </a>
     </div>
